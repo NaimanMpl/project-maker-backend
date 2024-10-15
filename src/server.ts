f4e7032505b87043138cbf83a8eddd64b52f8bd8
Zook-coder <zooklenon11@gmail.com>
2024-10-15 16:41:32 +0200

    chore(deps:ws): added ws package

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import WebSocket from 'ws';
+
+// const wss = new WebSocket.Server({ port: 8080 });
 
 dotenv.config();
 
@@ -8,6 +11,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.status(200).json({ messsage: "Hello, World!" });
 });
+
+
 
 app.listen(process.env.PORT, () => {
   console.log("Server listening at port: " + process.env.PORT);
