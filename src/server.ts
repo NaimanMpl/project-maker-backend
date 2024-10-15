6e9c7bf7e08ba6ff23fd237fbd85c186c136aad1
Zook-coder <zooklenon11@gmail.com>
2024-10-15 16:31:35 +0200

    fix: add .env functionnalities and fix log messages

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json({ messsage: "ello, World!" });
+  res.status(200).json({ messsage: "Hello, World!" });
 });
 
 app.listen(process.env.PORT, () => {
-  console.log("Server listening at port 3000");
+  console.log("Server listening at port: " + process.env.PORT);
 });
 
 export default app;
