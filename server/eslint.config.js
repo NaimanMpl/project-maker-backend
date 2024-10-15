56b05e2d8cd8d635d0ee6570187720f71e6906f8
Naïman <85957227+NaimanMpl@users.noreply.github.com>
2024-10-15 10:48:36 +0200

    chore: setup express server and ci (#1)

---
+++ server/eslint.config.js
@@ -0,0 +1,13 @@
+import pluginJs from "@eslint/js";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  { files: ["./src/**/*.{ts}"] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettier,
+  { ignores: ["./dist/**/*"] },
+];
