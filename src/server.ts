9343ec49d8d4550f99fe5d826ece8885c8fd5793
Naiman <85957227+NaimanMpl@users.noreply.github.com>
2024-10-15 11:49:38 +0200

    chore: added vercel json config file

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,8 @@ app.get("/", (req, res) => {
   res.status(200).json({ messsage: "ello, World!" });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server listening at port 3000");
 });
+
+export default app;
