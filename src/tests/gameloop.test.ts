1ea0f2fb56ddf9d6b6f22a289354bee8677a8cf7
Naiman <85957227+NaimanMpl@users.noreply.github.com>
2024-10-16 21:23:32 +0200

    test(gameloop): added gameloop timer test case

---
+++ src/tests/gameloop.test.ts
@@ -0,0 +1,34 @@
+import { GameState } from "@models/gamestate";
+import ioc, { Socket as ClientSocket } from "socket.io-client";
+import { game, io, server } from "../server";
+
+describe("GameLoop", () => {
+  let clientSocket: ClientSocket;
+
+  beforeAll((done) => {
+    server.listen(3001, () => {
+      clientSocket = ioc("http://localhost:3001");
+      clientSocket.on("connect", done);
+    });
+  });
+
+  afterAll((done) => {
+    clientSocket.close();
+    server.close(done);
+    io.close();
+  });
+
+  it("should send gamestate according to the tick rate", (done) => {
+    expect(game.state.timer).toBe(0);
+
+    clientSocket.on("gamestate", (msg) => {
+      const gamestate: GameState = JSON.parse(msg);
+
+      expect(gamestate.status).toBe("LOBBY");
+      expect(gamestate.loops).toBe(0);
+      expect(gamestate.timer).toBe(0.05); // (1 / tickRate) = 1 / 20 = 0.05
+
+      done();
+    });
+  });
+});
