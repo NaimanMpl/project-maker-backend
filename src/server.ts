d9927740ee69c8de16a30a3815e6d7451215c16a
Naïman <85957227+NaimanMpl@users.noreply.github.com>
2024-10-16 14:38:43 +0200

    feat(sockets): added web socket server (#7)
    Co-authored-by: Zook-coder <zooklenon11@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,45 @@
+import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.status(200).json({ messsage: "ello, World!" });
+  res.status(200).json({ message: "WebSocket Server Hello World" });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log("Server listening at port 3000");
+const PORT = process.env.PORT ?? 3001;
+const server = http.createServer(app);
+
+// Create a WebSocket server by passing the HTTP server instance to ws
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
 });
 
-export default app;
+io.on("connection", (socket) => {
+  console.log("Client connected");
+
+  // Handle messages from clients
+  socket.on("message", (message: string) => {
+    console.log(`Received message: ${message}`);
+    socket.send(JSON.stringify({ message }));
+  });
+
+  // Handle client disconnect
+  socket.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`HTTP server started on port ${PORT}`);
+});
