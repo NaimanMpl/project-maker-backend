f2c89c7693a8def3d4afa937f610a9cc19a21442
Axel <117041590+Drag0nQQ@users.noreply.github.com>
2024-10-21 10:54:05 +0200

    fix: fixed toast logout

--- src/handlers/logout.handler.ts
+++ src/handlers/logout.handler.ts
@@ -1,4 +1,5 @@
 import { Socket } from "socket.io";
+import { logger } from "../logger";
 import { Player } from "../models/player";
 import { game, io } from "../server";
 import { MessageHandler } from "./handler";
@@ -11,11 +12,18 @@ export class LogoutHandler extends MessageHandler {
   handleMessage(message: string): void {
     const payload: { id: string } = JSON.parse(message);
     const { id } = payload;
+    const logoutPlayer = game.players[id];
+
+    if (!logoutPlayer) {
+      logger.warn(`L'Id du joueur ${id} n'existe pas.`);
+      return;
+    }
+
     delete game.players[id];
     delete game.sockets[id];
-    const response: { players: Player[]; logoutPlayerId: string } = {
+    const response: { players: Player[]; logoutPlayer: Player } = {
       players: Object.values(game.players),
-      logoutPlayerId: id,
+      logoutPlayer: logoutPlayer,
     };
     io.to("lobby").emit("logoutplayer", JSON.stringify(response));
   }
