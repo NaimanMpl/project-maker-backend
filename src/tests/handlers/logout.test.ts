518e1e9678933751c3f74d23770be37f66e8e5a1
Axel <117041590+Drag0nQQ@users.noreply.github.com>
2024-10-21 11:19:49 +0200

    fix: fixed toast logout (#18)

--- src/tests/handlers/logout.test.ts
+++ src/tests/handlers/logout.test.ts
@@ -1,5 +1,6 @@
 import { Socket as ServerSocket } from "socket.io";
 import ioc, { Socket as ClientSocket } from "socket.io-client";
+import { logger } from "../../logger";
 import { Player } from "../../models/player";
 import { game, io, server } from "../../server";
 
@@ -52,10 +53,36 @@ describe("Logout Handler", () => {
     clientSocket.on("logoutplayer", (message) => {
       const {
         players,
-        logoutPlayerId,
-      }: { players: Player[]; logoutPlayerId: string } = JSON.parse(message);
+        logoutPlayer,
+      }: { players: Player[]; logoutPlayer: Player } = JSON.parse(message);
       expect(players).toEqual([]);
-      expect(logoutPlayerId).toEqual("1");
+      expect(logoutPlayer).toEqual({
+        id: "1",
+        name: "John",
+        type: "WEB",
+      });
+      done();
+    });
+  });
+
+  it("should warn if the player id doesn't exist", (done) => {
+    const loggerSpy = jest.spyOn(logger, "warn");
+
+    clientSocket.emit(
+      "logout",
+      JSON.stringify({
+        id: "1",
+      }),
+    );
+
+    serverSocket.on("logout", (message) => {
+      const { id }: { id: string } = JSON.parse(message);
+
+      expect(id).toEqual("1");
+      expect(game.players).toEqual({});
+
+      expect(loggerSpy).toHaveBeenCalledWith("L'Id du joueur 1 n'existe pas.");
+
       done();
     });
   });
