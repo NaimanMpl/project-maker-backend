b3ca3d0581c972f823b4d0167dd4ca4efccd665d
NaimanMpl <naimanmpl@gmail.com>
2024-10-28 12:20:50 +0100

    feat(items): added required password to cancel bomb

--- src/handlers/itemcancel.handler.ts
+++ src/handlers/itemcancel.handler.ts
@@ -3,14 +3,16 @@ import {
   NOT_A_PLAYER,
   UNAUTHORIZED,
   UNKNOWN_ITEM,
+  WRONG_PASSWORD,
 } from "../models/gameerror";
+import { Bomb } from "../models/items/bomb.item";
 import { DEFAULT_CANCEL_COOLDOWN } from "../models/player";
 import { game, io } from "../server";
 import { MessageHandler } from "./handler";
 
 export class ItemCancelHandler extends MessageHandler {
   handleMessage(message: string): void {
-    const { id, itemId } = JSON.parse(message);
+    const { id, itemId, password } = JSON.parse(message);
     const player = game.players[id];
 
     if (!player) {
@@ -33,6 +35,19 @@ export class ItemCancelHandler extends MessageHandler {
       this.socket.emit("error", JSON.stringify(UNKNOWN_ITEM));
       return;
     }
+
+    if (item instanceof Bomb) {
+      if (item.password !== password) {
+        player.blind = true;
+        setTimeout(() => {
+          player.blind = false;
+          this.socket.emit("player:unblind", undefined);
+        }, 2000);
+        this.socket.emit("error", JSON.stringify(WRONG_PASSWORD));
+        return;
+      }
+    }
+
     const owner = item.owner;
     this.socket.emit("item:cancel:success", JSON.stringify(owner));
 
--- src/models/items/bomb.item.ts
+++ src/models/items/bomb.item.ts
@@ -2,7 +2,17 @@ import { Player } from "../player";
 import { Item } from "./item";
 import { v4 as uuid4 } from "uuid";
 
+export const PHRASE_PASS: string[] = [
+  "Couper le fil rouge",
+  "Le fil bleu est sûr",
+  "LA BETE EST LA",
+  "LA BOMBE EST LA",
+  "C'est maintenant ou jamais",
+];
+
 export class Bomb extends Item {
+  password: string;
+
   constructor(coords: { x: number; y: number; z: number }) {
     super({
       id: uuid4(),
@@ -14,6 +24,7 @@ export class Bomb extends Item {
       castingTime: 1,
       duration: 15,
     });
+    this.password = PHRASE_PASS[Math.floor(Math.random() * PHRASE_PASS.length)];
   }
 
   /* istanbul ignore next */
